299064501b1cd6b5a0ea07e84b0253a92ac81996
Nikola Krstevski <36802068+nikolaesp@users.noreply.github.com>
2022-11-15 14:47:35 +0100

    iniziato stampare lista con while

---
+++ script.js
@@ -0,0 +1,6 @@
+let arrayspesa = ["1kg patate","4 cipolle","11 uova","steca costini","deodorante","candegina","3l Vino rosso"];
+let h = 0;
+while(h<arrayspesa.length){
+    console.log(arrayspesa[h++]);
+}
+
