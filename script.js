a44702fdcc6b2584b726c141a1b8158d9d5e8335
Nikola Krstevski <36802068+nikolaesp@users.noreply.github.com>
2022-11-15 15:17:36 +0100

    add item prova

--- script.js
+++ script.js
@@ -3,6 +3,19 @@ let h = 0;
 while(h<arrayspesa.length){
     console.log(arrayspesa[h++]);
 }
+console.log(`\n\n`);
+
+
+
+let additembtn = document.getElementById("btnadditem");
+additembtn.addEventListener('click',function(){
+    let item = document.getElementById("inputitem").value;
+    arrayspesa.push(item);
+});
+
+
+
+
 
 for(let i=0;i<arrayspesa.length;i++){
     console.log(arrayspesa[i]);
